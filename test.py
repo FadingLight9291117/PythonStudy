99a6fba099ed7028c935a3eecfe7ac9e7749e756
FadingLight <FadingLight9291117@126.com>
2019-05-31 13:41:42 +0800

    linux

--- test.py
+++
@@ -1,4 +0,0 @@
-import  jieba
-a="我是一名大学生"
-b=jieba.lcut(a)
-print(b)
--- 算法设计/迭代算法——递推法/兔子繁殖问题（3）——斐波拉契数列.py
+++ 算法设计/迭代算法——递推法/兔子繁殖问题（3）——斐波拉契数列.py
@@ -1,12 +1,12 @@
 '''
     算法3：无需引入第三个变量c
 '''
-a=1
-b=1
+a = 1
+b = 1
 print(a)
 print(b)
 for i in range(5):
-    a=a+b
+    a = a+b
     print(a)
-    b=a+b
+    b = a+b
     print(b)
